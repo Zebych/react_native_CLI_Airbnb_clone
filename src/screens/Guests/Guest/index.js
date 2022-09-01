ca6f8f838d2fbab8f980ba09435ee7cf089ac4b4
Zebych <81613417+Zebych@users.noreply.github.com>
2022-09-01 17:01:04 +0300

    guests screen

---
+++ src/screens/Guests/Guest/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './styles';
+
+const GuestsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Гости</Text>
+    </View>
+  );
+};
+
+export default GuestsScreen;
